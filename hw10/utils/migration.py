e357a2a14930683e788c4962dc86d3c30bc976da
lexxai <lexxai@users.noreply.github.com>
2023-10-23 02:10:24 +0300

    migrate to pg quotes and tags

--- hw10/utils/migration.py
+++ hw10/utils/migration.py
@@ -6,40 +6,40 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hw10.settings")
 
 django.setup()
 
-from quotes.models import Quote, Tag, Author #noqa
-from quotes.utils import get_mongodb 
+from quotes.models import Quote, Tag, Author  # noqa
+from quotes.utils import get_mongodb
+
 
 def import_records():
     db = get_mongodb()
     authors = db.authors.find()
-    
+
     for author in authors:
         author_data = {
             "fullname": author["fullname"],
             "born_date": author["born_date"],
             "born_location": author["born_location"],
-            "description": author["description"]
+            "description": author["description"],
         }
         Author.objects.get_or_create(**author_data)
 
     quotes = db.quotes.find()
-    tags = set()
     for quote in quotes:
+        tags = []
         for tag in quote.get("tags"):
             if tag:
-                tags.add(tag)
+                t, *_ = Tag.objects.get_or_create(name=tag)
+                tags.append(t)
 
-    tags_obj = []
-    for tag in tags:
-        # print(f"{tag=}")
-        t, *_ = Tag.objects.get_or_create(name=tag)
-        tags_obj.append(t)
-    
-    # print(tags_obj)
+        exist_quote = bool(len(Quote.objects.filter(quote=quote["quote"])))
+        if not exist_quote:
+            author = db.authors.find_one({"_id": quote["author"]})
+            a = Author.objects.get(fullname=author["fullname"])
+            quote_data = {"author": a, "quote": quote["quote"]}
+            q = Quote.objects.create(**quote_data)
+            for tag in tags:
+                q.tags.add(tag)
 
 
 if __name__ == "__main__":
     import_records()
-
- 
-
