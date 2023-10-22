9f9cdd20501a4b84d971b92405dab78b887b72b5
lexxai <lexxai@users.noreply.github.com>
2023-10-22 17:24:30 +0300

    init django

--- hw10/hw10/settings.py
+++ hw10/hw10/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+load_dotenv(BASE_DIR.parent.joinpath(".env"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "quotes",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -73,10 +80,24 @@ WSGI_APPLICATION = "hw10.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "XXXXXXXX")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "127.0.0.1")
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": "5432",
     }
 }
 
--- scipts/docker_run_db.cmd
+++ scipts/docker_run_db.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-docker run --name noteapp-postgres -p 5432:5432 -e POSTGRES_PASSWORD=567234 -d postgres
+docker run --name hw10-postgres -p 5432:5432 --env-file ../.env -d postgres
 
 
                     
