a3bf0b27d99e2a339076467129e7bc4e1a389116
lexxai <lexxai@users.noreply.github.com>
2023-10-23 15:46:56 +0300

    authors, quotes list

--- hw10/quotes/urls.py
+++ hw10/quotes/urls.py
@@ -6,4 +6,7 @@ app_name = 'quotes'
 urlpatterns = [
     path('', views.main, name='main'),
     path('<int:page>', views.main, name='main_paginate'),  
+    path('author/<str:author>', views.author, name='author'),  
+    path('tag/<str:tag>', views.tag, name='tag'),  
+    path('tag/<str:tag>/<int:page>', views.tag, name='tag_paginate'),  
 ]
--- hw10/quotes/views.py
+++ hw10/quotes/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
-from .utils import get_mongodb
+from .models import Quote, Author, Tag
+
 
 PER_PAGE = 2
 
+# def main_mongodb_version(request, page=1):
+#     from .utils import get_mongodb
+#     db = get_mongodb()
+#     quotes = db.quotes.find({}).limit(15)
+#     paginator = Paginator(list(quotes), per_page=PER_PAGE)
+
+#     context = {"quotes": paginator.page(page)}
+#     return render(request, "quotes/index.html", context)
+
+
 def main(request, page=1):
-    db = get_mongodb()
-    quotes = db.quotes.find({}).limit(15)
+    quotes = Quote.objects.all()
     paginator = Paginator(list(quotes), per_page=PER_PAGE)
 
     context = {"quotes": paginator.page(page)}
     return render(request, "quotes/index.html", context)
+
+
+def author(request, author: str):
+    try:
+        author = Author.objects.get(fullname=author)
+    except:
+        author = None
+    context = {"author": author}
+    return render(request, "quotes/author.html", context)
+
+def tag(request, tag: str, page: int = 1):
+    quotes = Tag.objects.get(name=tag)
+    paginator = Paginator(list(quotes), per_page=PER_PAGE)
+
+    context = {"tag": paginator.page(page)}
+    return render(request, "quotes/tag.html", context)
